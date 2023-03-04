407e25073cc716698525cb69a6acfa267facb4a2
kauawb02 <kauabatista254@gmail.com>
2023-03-04 04:30:54 -0300

    Fix: Modification in error handling listing method, user register method created and corrected, withdrawn then

--- src/database/models/user.ts
+++ src/database/models/user.ts
@@ -12,21 +12,24 @@ export class modelUser {
 					PASSWORD: dadosUser.password,
 					PHONE: dadosUser.phone,
 					DT_CREATION: connection.raw(`CURRENT_TIMESTAMP`)
-				}).then((data: any) => {
-					return data;
 				})
-
 		} catch (e) {
 			throw { message: `Ocorreu algo de errado na criação de usuário [${e}]` }
 		}
 	}
 
 	public async listUsers() {
+		let users: Array<object> = [];
 		try {
-			let users = await connection('TB_USER').select('*');
-			return users;
+
+			users = await connection('TB_USER')
+				.select('ID_USER', 'NAME', 'EMAIL', 'CPF', 'PHONE', 'DT_CREATION');
+
 		} catch (e) {
-			throw { message: `Ocorreu algo de errado na listagem de usuarios [${e}]` }
+			console.log(e)
+			throw { message: `Ocorreu algo de errado na listagem de usuarios` };
+
 		}
+		return users;
 	}
 }
