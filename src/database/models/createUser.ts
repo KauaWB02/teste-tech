25506652c2a4ba3298e298d9c626288064ee9f28
kauawb02 <kauabatista254@gmail.com>
2023-03-04 02:46:36 -0300

    Fix: Change in file naming

--- src/database/models/createUser.ts
+++
@@ -1,8 +0,0 @@
-import { connection } from "../connection";
-
-export class modelCreateUser {
-    public async createUser() {
-       let teste = await connection('TB_USER').select('*');
-       return teste;
-    }
-}
