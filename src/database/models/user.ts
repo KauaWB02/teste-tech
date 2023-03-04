6b9c917264c95b19c043cdb4dfe120969ad23283
kauawb02 <kauabatista254@gmail.com>
2023-03-04 03:16:27 -0300

    Feat: New created method of creating new user

--- src/database/models/user.ts
+++ src/database/models/user.ts
@@ -1,8 +1,32 @@
 import { connection } from "../connection";
 
 export class modelUser {
-    public async listUsers() {
-       let users = await connection('TB_USER').select('*');
-       return users;
-    }
+
+	public async createUser(dadosUser: createUser) {
+		try {
+			await connection('TB_USER')
+				.insert({
+					NAME: dadosUser.name,
+					EMAIL: dadosUser.email,
+					CPF: dadosUser.cpf,
+					PASSWORD: dadosUser.password,
+					PHONE: dadosUser.phone,
+					DT_CREATION: connection.raw(`CURRENT_TIMESTAMP`)
+				}).then((data: any) => {
+					return data;
+				})
+
+		} catch (e) {
+			throw { message: `Ocorreu algo de errado na criação de usuário [${e}]` }
+		}
+	}
+
+	public async listUsers() {
+		try {
+			let users = await connection('TB_USER').select('*');
+			return users;
+		} catch (e) {
+			throw { message: `Ocorreu algo de errado na listagem de usuarios [${e}]` }
+		}
+	}
 }
