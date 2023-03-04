9f14c839a5e2f8467bbbf8139a51c4119b1c1841
kauawb02 <kauabatista254@gmail.com>
2023-03-04 02:47:29 -0300

    Fix: Modification in the name of the variable that is being tested

--- src/controlls/systemControlls/user.ts
+++ src/controlls/systemControlls/user.ts
@@ -1,10 +1,10 @@
 import { Request, Response } from "express";
-import { modelCreateUser } from "../../database/models/createUser";
+import { modelUser } from '../../database/models/user'
 
 export class controllUser {
     public async listUsers(request: Request, response: Response) {
-        let modelUser = new modelCreateUser()
-        let teste = await modelUser.createUser();
-        response.status(200).send(teste)
+        let modelUsers = new modelUser();
+        let list = await modelUsers.listUsers();
+        response.status(200).send(list)
     }
 }
