c988bb339d7b2449c3f9ca87309ec4456bffff63
kauawb02 <kauabatista254@gmail.com>
2023-03-04 13:49:55 -0300

    Fix: Change in quote standardization

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,5 @@
 import express from 'express';
-import { server } from "./config/app";
+import { server } from './config/app';
 import { routes } from './controlls/routesControlls/routeControll';
 
 
