771269bd09b962c993555727e119b1e14fd09173
kauawb02 <kauabatista254@gmail.com>
2023-03-05 19:43:12 -0300

    Fix: Added some more parameters in the interface

---
+++ src/interface/createUserProfile.ts
@@ -0,0 +1,10 @@
+export interface userProfile {
+    name?: string,
+    email?: string,
+    cpf?: string,
+    password?: string,
+    phone?: string,
+    dtCreation?: Date,
+    idUser?: number,
+    isAdmin?: boolean,
+}
